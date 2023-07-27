923557e85ba3fa3c7e806e73bee0206f70ef41ee
Geethu <geethuaravind1994@gmail.com>
2023-07-27 19:37:33 +0530

    Changes

--- src/com/obsqura/SampleClass.java
+++ src/com/obsqura/SampleClass.java
@@ -4,5 +4,6 @@ public class SampleClass {
     public static void main(String[] args) {
         System.out.println("This is a sample class");
         System.out.println("Hello world");
+        System.out.println("New change");
     }
 }
